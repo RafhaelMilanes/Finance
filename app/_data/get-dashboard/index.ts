549d4d0f954705ee5ac35d0ebb9ff336262e1e13
Rafhael Milanes <rafhael.t3ch@gmail.com>
2025-01-23 13:24:16 -0300

    feat: implement user authentication for dashboard data fetching; redirect to current month if invalid

--- app/_data/get-dashboard/index.ts
+++ app/_data/get-dashboard/index.ts
@@ -1,9 +1,17 @@
 import { db } from "@/app/_lib/prisma";
 import { TransactionsType } from "@prisma/client";
 import { TotoalExpenseCategory, TransactionsPercentagePerType } from "./types";
+import { auth } from "@clerk/nextjs/server";
 
 export const getDashboard = async (month: string) => {
+  const { userId } = await auth();
+
+  if (!userId) {
+    throw new Error("Unauthorized");
+  }
+
   const where = {
+    userId,
     date: {
       gte: new Date(`2025-${month}-01`),
       lt: new Date(`2025-${month}-31`),
