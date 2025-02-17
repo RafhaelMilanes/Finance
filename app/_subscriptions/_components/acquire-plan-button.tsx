5ab75f2ed30b4f7ecc638ffa216ea152b70de2cf
Rafhael Milanes <rafhael.t3ch@gmail.com>
2025-02-17 15:59:29 -0300

    remove component acquire plan

--- app/_subscriptions/_components/acquire-plan-button.tsx
+++ app/_subscriptions/_components/acquire-plan-button.tsx
@@ -1,24 +1,24 @@
-"use client";
+// "use client";
 
-import { Button } from "@/app/_components/ui/button";
-import { createStripeCheckout } from "../_actions/create-stripe-checkout";
-import { loadStripe } from "@stripe/stripe-js";
+// import { Button } from "@/app/_components/ui/button";
+// import { createStripeCheckout } from "../_actions/create-stripe-checkout";
+// import { loadStripe } from "@stripe/stripe-js";
 
-const AcquirePlanButton = () => {
-  const handlePlanCLick = async () => {
-    const { sessionId } = await createStripeCheckout();
-    const stripe = await loadStripe(
-      process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
-    );
+// const AcquirePlanButton = () => {
+//   const handlePlanCLick = async () => {
+//     const { sessionId } = await createStripeCheckout();
+//     const stripe = await loadStripe(
+//       process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string,
+//     );
 
-    await stripe?.redirectToCheckout({ sessionId });
-  };
+//     await stripe?.redirectToCheckout({ sessionId });
+//   };
 
-  return (
-    <Button className="w-full rounded-xl font-bold" onClick={handlePlanCLick}>
-      Adiquirir plano
-    </Button>
-  );
-};
+//   return (
+//     <Button className="w-full rounded-xl font-bold" onClick={handlePlanCLick}>
+//       Adiquirir plano
+//     </Button>
+//   );
+// };
 
-export default AcquirePlanButton;
+// export default AcquirePlanButton;
